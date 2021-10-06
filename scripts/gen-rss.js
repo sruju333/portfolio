f147207508837995758ed3940e909c881a607f95
Srujana N <sruju333@gmail.com>
2021-10-06 22:28:14 +0530

    added name to blog

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Srujana',
+    site_url: 'https://portfolio-sruju333.vercel.app',
+    feed_url: 'https://portfolio-sruju333.vercel.app/posts'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
